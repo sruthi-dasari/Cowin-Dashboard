f536a6d82e5cad29ad03992e8988a23e94ef8e5d
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-03-01 23:04:59 +0530

    Update index.js

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -6,7 +6,7 @@ import {
   XAxis,
   YAxis,
   Legend,
-  ResponsiveContainer,
+  //   ResponsiveContainer,
 } from 'recharts'
 
 const VaccinationCoverage = props => {
@@ -24,36 +24,38 @@ const VaccinationCoverage = props => {
   return (
     <div className="vaccination-coverage-container">
       <h1 className="vaccination-coverage-heading">Vaccination Coverage</h1>
-      <ResponsiveContainer width="100%" height={500}>
-        <BarChart
-          data={data}
-          margin={{
-            top: 5,
+      {/* <ResponsiveContainer width="100%" height={500}> */}
+      <BarChart
+        data={data}
+        margin={{
+          top: 5,
+        }}
+        height={300}
+        width={1000}
+      >
+        <XAxis
+          dataKey="vaccineDate"
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 1,
           }}
-        >
-          <XAxis
-            dataKey="vaccineDate"
-            tick={{
-              stroke: '#6c757d',
-              strokeWidth: 1,
-            }}
-          />
-          <YAxis
-            tickFormatter={DataFormatter}
-            tick={{
-              stroke: '#6c757d',
-              strokeWidth: 0.5,
-            }}
-          />
-          <Legend
-            wrapperStyle={{
-              padding: 30,
-            }}
-          />
-          <Bar dataKey="dose1" name="Dose1" fill="#5a8dee" barSize="20%" />
-          <Bar dataKey="dose2" name="Dose2" fill="#f54394" barSize="20%" />
-        </BarChart>
-      </ResponsiveContainer>
+        />
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 0.5,
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            padding: 30,
+          }}
+        />
+        <Bar dataKey="dose1" name="Dose1" fill="#5a8dee" barSize="20%" />
+        <Bar dataKey="dose2" name="Dose2" fill="#f54394" barSize="20%" />
+      </BarChart>
+      {/* </ResponsiveContainer> */}
     </div>
   )
 }
