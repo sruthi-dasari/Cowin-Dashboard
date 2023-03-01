e51a4ce3eaa8ac82c6e5d99f169f31c4a6a62325
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-03-01 23:05:21 +0530

    Update index.js

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,5 +1,5 @@
 import './index.css'
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 
 const VaccinationByAge = props => {
   const {data} = props
@@ -7,30 +7,30 @@ const VaccinationByAge = props => {
   return (
     <div className="vaccination-by-age-container">
       <h1 className="vaccination-by-age-heading">Vaccination by Age</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="70%"
-            cy="40%"
-            data={data}
-            startAngle={0}
-            endAngle={360}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="18-44" fill="#2d87bb" />
-            <Cell name="44-60" fill="#a3df9f" />
-            <Cell name="Above 60" fill="#64c2a6" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="horizontal"
-            verticalAlign="bottom"
-            align="center"
-          />
-        </PieChart>
-      </ResponsiveContainer>
+      {/* <ResponsiveContainer width="100%" height={300}> */}
+      <PieChart height={300} width={1000}>
+        <Pie
+          cx="70%"
+          cy="40%"
+          data={data}
+          startAngle={0}
+          endAngle={360}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#2d87bb" />
+          <Cell name="44-60" fill="#a3df9f" />
+          <Cell name="Above 60" fill="#64c2a6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+        />
+      </PieChart>
+      {/* </ResponsiveContainer> */}
     </div>
   )
 }
