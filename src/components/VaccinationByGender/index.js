e42e0a0eb8d95bc1a3b60e30291a3dce891fce05
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-03-01 23:05:46 +0530

    Update index.js

--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,5 +1,5 @@
 import './index.css'
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 
 const VaccinationByGender = props => {
   const {data} = props
@@ -7,30 +7,32 @@ const VaccinationByGender = props => {
   return (
     <div className="vaccination-by-gender-container">
       <h1 className="vaccination-by-gender-heading">Vaccination by gender</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="70%"
-            cy="40%"
-            data={data}
-            startAngle={0}
-            endAngle={360}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="Male" fill="#f54394" />
-            <Cell name="Female" fill="#5a8dee" />
-            <Cell name="Others" fill="#2cc6c6" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="horizontal"
-            verticalAlign="bottom"
-            align="center"
-          />
-        </PieChart>
-      </ResponsiveContainer>
+      {/* <ResponsiveContainer width="100%" height={300}> */}
+      <PieChart>
+        <Pie
+          cx="70%"
+          cy="40%"
+          data={data}
+          startAngle={0}
+          endAngle={360}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
+          height={300}
+          width={1000}
+        >
+          <Cell name="Male" fill="#f54394" />
+          <Cell name="Female" fill="#5a8dee" />
+          <Cell name="Others" fill="#2cc6c6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+        />
+      </PieChart>
+      {/* </ResponsiveContainer> */}
     </div>
   )
 }
