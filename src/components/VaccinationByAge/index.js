442cff159eebc3f53f463c704b83d3ef38911fe7
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-03-04 11:02:03 +0530

    Update index.js

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -10,12 +10,12 @@ const VaccinationByAge = props => {
       {/* <ResponsiveContainer width="100%" height={300}> */}
       <PieChart height={300} width={1000}>
         <Pie
-          cx="70%"
+          cx="50%"
           cy="40%"
           data={data}
           startAngle={0}
           endAngle={360}
-          innerRadius="40%"
+          innerRadius="0%"
           outerRadius="70%"
           dataKey="count"
         >
